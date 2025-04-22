1c6b36f88e340cd977deacad6967bd4b648d1ea9
Gleb <jimgearalt@gmail.com>
2025-04-22 15:50:15 +0300

    123

--- js/task-3.js
+++ js/task-3.js
@@ -8,6 +8,10 @@ function filterArray(numbers, value) {
   return newArray;
 }
 
+function() {
+  console.log(numbers)
+}
+
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
 console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
